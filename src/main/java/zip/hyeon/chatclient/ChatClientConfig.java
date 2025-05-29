0d29428d6b51734d0fc050748456ed92dd8ec0f2
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-29 16:46:02 +0900

    :sparkles: ChatMemory 기능 추가

---
+++ src/main/java/zip/hyeon/chatclient/ChatClientConfig.java
@@ -0,0 +1,54 @@
+package zip.hyeon.chatclient;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.ChatMemoryRepository;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.ai.chat.memory.repository.jdbc.JdbcChatMemoryRepository;
+import org.springframework.ai.chat.memory.repository.jdbc.MysqlChatMemoryRepositoryDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@Configuration
+@RequiredArgsConstructor
+public class ChatClientConfig {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    /**
+     * ChatClient Build
+     */
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
+
+        return chatClientBuilder
+                .defaultSystem("너는 사용자가 요청한 상품 정보 또는 표현에 대해 유사항 상품을 추천하는 AI 에이전트야.")
+                .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory()).build())
+                .build();
+    }
+
+    /**
+     * ChatMemory Build
+     */
+    @Bean
+    public ChatMemory chatMemory(){
+        return MessageWindowChatMemory.builder()
+                .chatMemoryRepository(chatMemoryRepository())
+                .maxMessages(10)
+                .build();
+    }
+
+    /**
+     * ChatMemoryRepository Build
+     */
+    @Bean
+    public ChatMemoryRepository chatMemoryRepository(){
+        return JdbcChatMemoryRepository.builder()
+                .jdbcTemplate(jdbcTemplate)
+                .dialect(new MysqlChatMemoryRepositoryDialect())
+                .build();
+    }
+}
--- src/main/java/zip/hyeon/chatclient/ChatClientController.java
+++ src/main/java/zip/hyeon/chatclient/ChatClientController.java
@@ -1,6 +1,7 @@
 package zip.hyeon.chatclient;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -10,16 +11,18 @@ public class ChatClientController {
 
     private final ChatClient chatClient;
 
-    public ChatClientController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public ChatClientController(ChatClient chatClient) {
+        this.chatClient = chatClient;
     }
 
     /**
      * 논스트리밍 요청(모든 답변이 한번에 옴)
      */
     @GetMapping("/ai")
-    String generation(String userInput) {
+    String generation(String userInput, String userId) {
+
         return this.chatClient.prompt()
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID, userId))
                 .user(userInput)
                 .call()
                 .content();
@@ -29,8 +32,9 @@ public class ChatClientController {
      * 스트리밍 요청(답변 일부가 생성되자마자 옴)
      */
     @GetMapping(value = "/ai/stream", produces = "text/plain;charset=UTF-8")
-    Flux<String> streamGeneration(String userInput) {
+    Flux<String> streamGeneration(String userInput, String userId) {
         return this.chatClient.prompt()
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID, userId))
                 .user(userInput)
                 .stream()
                 .content();
