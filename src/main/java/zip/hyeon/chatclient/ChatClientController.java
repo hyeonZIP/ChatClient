03b28e2bc5c7585d39b71f6c4807935729af1cfa
hyeonZIP <139322256+hyeonZIP@users.noreply.github.com>
2025-05-29 10:33:55 +0900

    :sparkles: 스트리밍 방식 요청 기능 추가

--- src/main/java/zip/hyeon/chatclient/ChatClientController.java
+++ src/main/java/zip/hyeon/chatclient/ChatClientController.java
@@ -3,21 +3,30 @@ package zip.hyeon.chatclient;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatClientController {
 
     private final ChatClient chatClient;
 
-    public ChatClientController(ChatClient.Builder chatClientBuilder){
+    public ChatClientController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
 
     @GetMapping("/ai")
-    String generation(String userInput){
+    String generation(String userInput) {
         return this.chatClient.prompt()
                 .user(userInput)
                 .call()
+                .content();
+    }
+
+    @GetMapping(value = "/ai/stream", produces = "text/plain;charset=UTF-8")
+    Flux<String> streamGeneration(String userInput) {
+        return this.chatClient.prompt()
+                .user(userInput)
+                .stream()
                 .content();
     }
 }
